fa22dea889dad29699835f31315b0fa54cb4f024
cs-mac <halfmoonvic@gmail.com>
2021-05-04 11:55:03 +0800

    chore: eslint prettier babel editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': ['warn' /* { semi: true } */],
+  },
+};
