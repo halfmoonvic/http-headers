455c327df49f571920845cf0b95b909bdeed8360
cs-mac <halfmoonvic@gmail.com>
2021-05-10 15:35:28 +0800

    transfer-encoding

--- server/express.js
+++ server/express.js
@@ -3,8 +3,18 @@ const app = express();
 
 const router = express.Router();
 
-router.get('/simple/test', (req, res) => {
-  res.send('Express Hello World !');
+router.get('/transfer-encoding', (req, res) => {
+  res.header('Content-Type', 'text/html; charset=utf8');
+  res.header('Transfer-Encoding', 'chunked');
+
+  res.write('<p>express 第一次传输</p>');
+  setTimeout(() => {
+    res.write('<p>express 第二次传输</p>');
+    setTimeout(() => {
+      res.write('<p>express 第三次传输</p>');
+      res.end();
+    }, 1000);
+  }, 2000);
 });
 
 app.use(router);
--- server/native.js
+++ server/native.js
@@ -2,11 +2,23 @@ const http = require('http');
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-  if (req.url.indexOf('simple/test') > -1) {
-    res.end('Native Hello World !');
-  } else {
-    res.end('Native Not Found');
+  if (req.url.indexOf('/transfer-encoding') > -1) {
+    res.setHeader('Content-Type', 'text/html; charset=utf8');
+    res.setHeader('Transfer-Encoding', 'chunked');
+
+    res.write('<p>native 第一次传输</p>');
+    setTimeout(() => {
+      res.write('<p>native 第二次传输</p>');
+      setTimeout(() => {
+        res.write('<p>native 第三次传输</p>');
+        res.end();
+      }, 1000);
+    }, 2000);
+
+    return;
   }
+
+  res.end('Not Found');
 });
 
 const port = 3000;
